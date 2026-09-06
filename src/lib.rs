7f5447435325c015bf1027aa8bcf9abd8e0afee7
Anthony Emmanuella Mmasinachi <anthonyemmanuella297@gmail.com>
2026-09-06 07:39:46 -0700

    Expose ATLAS proxy runtime API

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ pub mod http;
 pub mod proxy;
 
 pub use http::{parse_request, HttpRequest, ParseError};
-pub use proxy::{proxy_connection, ProxyError};
+pub use proxy::{proxy_connection, proxy_connection_with_state, ProxyConfig, ProxyError, ProxyMetrics, ProxyState};
