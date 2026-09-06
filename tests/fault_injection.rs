511d183c214ce0cc64da915c7a77a134b987e55f
Anthony Emmanuella Mmasinachi <anthonyemmanuella297@gmail.com>
2026-09-06 11:07:02 -0700

    Fix Rust formatting in fault injection tests

--- tests/fault_injection.rs
+++ tests/fault_injection.rs
@@ -9,7 +9,7 @@ use tokio::net::TcpListener;
 #[tokio::test]
 async fn unavailable_backend_returns_503() {
     let proxy_listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
-    let proxy_addr = proxy_listener.local_addr().unwrap();
+    let proxy_addr = proxy_listener.local_addr().await.unwrap();
     let backend = unused_local_address();
     let config = ProxyConfig {
         backends: vec![backend],
@@ -26,7 +26,10 @@ async fn unavailable_backend_returns_503() {
     });
 
     let mut client = tokio::net::TcpStream::connect(proxy_addr).await.unwrap();
-    client.write_all(b"GET / HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n").await.unwrap();
+    client
+        .write_all(b"GET / HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n")
+        .await
+        .unwrap();
     let mut response = Vec::new();
     client.read_to_end(&mut response).await.unwrap();
     assert!(response.starts_with(b"HTTP/1.1 503 Service Unavailable\r\n"));
@@ -36,7 +39,7 @@ async fn unavailable_backend_returns_503() {
 #[tokio::test]
 async fn metrics_endpoint_is_available_without_upstream() {
     let proxy_listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
-    let proxy_addr = proxy_listener.local_addr().unwrap();
+    let proxy_addr = proxy_listener.local_addr().await.unwrap();
     let state = ProxyState::new(ProxyConfig::default());
 
     let server = tokio::spawn(async move {
@@ -45,7 +48,10 @@ async fn metrics_endpoint_is_available_without_upstream() {
     });
 
     let mut client = tokio::net::TcpStream::connect(proxy_addr).await.unwrap();
-    client.write_all(b"GET /metrics HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n").await.unwrap();
+    client
+        .write_all(b"GET /metrics HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n")
+        .await
+        .unwrap();
     let mut response = String::new();
     client.read_to_string(&mut response).await.unwrap();
     assert!(response.starts_with("HTTP/1.1 200 OK\r\n"));
@@ -54,5 +60,8 @@ async fn metrics_endpoint_is_available_without_upstream() {
 }
 
 fn unused_local_address() -> SocketAddr {
-    std::net::TcpListener::bind("127.0.0.1:0").unwrap().local_addr().unwrap()
+    std::net::TcpListener::bind("127.0.0.1:0")
+        .unwrap()
+        .local_addr()
+        .unwrap()
 }
